eac7e2dbd50ddfc6432c2121ef615db684358b2f
rogerongzy <610825661@qq.com>
2023-06-13 15:43:20 +0800

    updated on 230613

--- data_230609/only_test.py
+++ data_230609/only_test.py
@@ -1,15 +1,16 @@
 import random
+from tkinter import CENTER
 import numpy as np
 import matplotlib.pyplot as plt
 from cmath import exp
 
-### preset point coordinate ###
-ant_1 = np.array((0, 0))
-ant_2 = np.array((10, 0))
-ant_3 = np.array((20, 0))
-ant_4 = np.array((30, 0))
-
-pt_1 = np.array((25, 80)) # original pos
+### preset antenna coordinate, in metres ###
+ant_1 = np.array((-0.375, 0))
+ant_2 = np.array((-0.125, 0))
+ant_3 = np.array((0.125, 0))
+ant_4 = np.array((0.375, 0))
+WAVE_LENGTH = 0.232924707
+pt_1 = np.array((-0.3, 1)) # initial starting coordinate
 
 
 
@@ -18,67 +19,72 @@ colors = ['crimson', 'magenta', 'darkslateblue', 'violet', 'blue', 'steelblue', 
 
 
 
+
 def generate_random_pt(centre, radius):
-    new_pt = np.array((centre[0] + random.uniform(-radius,radius), centre[1] + random.uniform(-radius,radius)))
+    # new_pt = np.array((centre[0] + random.uniform(-radius, radius), centre[1] + random.uniform(-radius, radius))) # square space
+
+    random_radius = random.uniform(0, radius)
+    random_angle = random.uniform(0, 2 * np.pi)
+    new_pt = np.array((centre[0] +  random_radius * np.cos(random_angle), centre[1] + random_radius * np.sin(random_angle))) # circle space
+
     return new_pt
 
+
 def get_phasor(pt_tag):
-    phasor = np.array((np.linalg.norm(pt_tag - ant_1), np.linalg.norm(pt_tag - ant_2), np.linalg.norm(pt_tag - ant_3), np.linalg.norm(pt_tag - ant_4)))
-    return phasor
+    eu_distance = np.array((np.linalg.norm(pt_tag - ant_1), np.linalg.norm(pt_tag - ant_2), np.linalg.norm(pt_tag - ant_3), np.linalg.norm(pt_tag - ant_4)))
+    return eu_distance * 4 * np.pi / WAVE_LENGTH
+
 
 def phasor_normalized(phasor):
     phasor_temp = []
     for i in range(len(phasor)):
         phasor_temp.append(exp(-1j*(phasor[i] - phasor[0])))
-    # phasor_normalized = np.matrix(phasor_temp) # matrix
-    phasor_normalized = np.array(phasor_temp) # ndarray
-
+    # phasor_normalized = np.matrix(phasor_temp) # matrix version
+    phasor_normalized = np.array(phasor_temp) # array version
     return phasor_normalized
 
-# def cosine_similarity(complex_vec_1, complex_vec_2):
-#     num = np.linalg.norm(np.dot(complex_vec_1, complex_vec_2.getH()))
-#     den = np.linalg.norm(complex_vec_1) * np.linalg.norm(complex_vec_2)
-#     return num / den
 
-# another matching
 def cosine_similarity(complex_vec_1, complex_vec_2):
-    # num = np.linalg.norm(np.dot(complex_vec_1, complex_vec_2.getH()))
-    num = np.linalg.norm(np.dot(complex_vec_1, complex_vec_2.conjugate().T)) # abs is same to norm
+    # num = np.linalg.norm(np.dot(complex_vec_1, complex_vec_2.getH())) # used in matrix version, same result
+    num = np.linalg.norm(np.dot(complex_vec_1, complex_vec_2.conjugate().T)) # same using abs or norm
     # print(np.dot(complex_vec_1, complex_vec_2.conjugate().T), np.dot(complex_vec_1.conjugate().T, complex_vec_2)) # differnet in complex part, but same in norm/abs
     den = np.linalg.norm(complex_vec_1) * np.linalg.norm(complex_vec_2)
     return num / den
 
-def plot_candidates(pt_current, raduis, num_circular_split):
-    for i in range(num_circular_split):
-        pt_temp = np.array((pt_current[0] + raduis * np.cos(i * 2 * np.pi / num_circular_split), pt_current[1] + raduis * np.sin(i * 2 * np.pi / num_circular_split)))
+
+def plot_candidates(pt_current, radius, num_candidates):
+    for i in range(num_candidates):
+        pt_temp = np.array((pt_current[0] + radius * np.cos(i * 2 * np.pi / num_candidates), pt_current[1] + radius * np.sin(i * 2 * np.pi / num_candidates)))
         plt.scatter(pt_temp[0], pt_temp[1], c=colors[i], s=200)
     plt.scatter(pt_current[0], pt_current[1], c=colors[16], s=200)
 
-def candidate_generator(pt_current, pt_measured, raduis, num_circular_split):
+
+def candidate_generator(pt_current, pt_measured, radius, num_candidates):
     candidates = []
     result = []
     phasor_measured = get_phasor(pt_measured)
 
-    for i in range(num_circular_split):
-        # pt_temp = np.array((pt_current[0] + raduis * np.cos(i * 2 * np.pi / num_circular_split), pt_current[1] + raduis * np.sin(i * 2 * np.pi / num_circular_split))) # circulate
+    for i in range(num_candidates):
+        # pt_temp = np.array((pt_current[0] + radius * np.cos(i * 2 * np.pi / num_candidates), pt_current[1] + radius * np.sin(i * 2 * np.pi / num_candidates))) # circumference
 
-        # pt_temp = np.array((pt_current[0] + random.uniform(-raduis, raduis), pt_current[1] + random.uniform(-raduis, raduis))) # random
-        pt_temp = generate_random_pt(pt_current, raduis) # random
+        pt_temp = generate_random_pt(pt_current, radius)
 
         phasor_temp = get_phasor(pt_temp)
         sim_temp = cosine_similarity(phasor_normalized(phasor_measured), phasor_normalized(phasor_temp))
-        
+    
         candidates.append(pt_temp)
         result.append(sim_temp)
     
-    # add itself as a candidate, other than the circle, totally 9 candidates
-    pt_temp = pt_current
-    phasor_temp = get_phasor(pt_temp)
-    sim_temp = cosine_similarity(phasor_normalized(phasor_measured), phasor_normalized(phasor_temp))
+    
+    # add itself (circle centre) as a candidate other than the circle
+    ### if randomly generate candidates, no need to add itself
+    # pt_temp = pt_current
+    # phasor_temp = get_phasor(pt_temp)
+    # sim_temp = cosine_similarity(phasor_normalized(phasor_measured), phasor_normalized(phasor_temp))
 
-    candidates.append(pt_temp)
-    result.append(sim_temp)
-    # print(result)
+    # candidates.append(pt_temp)
+    # result.append(sim_temp)
+
 
     # 1, only returning the candidate with the biggest similarity
     idx = result.index(max(result))
@@ -153,16 +159,18 @@ def plot_hotspot():
 
 
 
+
+
 ###### phasor differenciate ######
 
-def candidate_generator_df(pt_current, pt_measured, raduis, num_circular_split):
+def candidate_generator_df(pt_current, pt_measured, radius, num_candidates):
     candidates = []
     result = []
     phasor_current = get_phasor(pt_current)
     phasor_measured = get_phasor(pt_measured)
 
-    for i in range(num_circular_split):
-        pt_temp = np.array((pt_current[0] + raduis * np.cos(i * 2 * np.pi / num_circular_split), pt_current[1] + raduis * np.sin(i * 2 * np.pi / num_circular_split)))
+    for i in range(num_candidates):
+        pt_temp = np.array((pt_current[0] + radius * np.cos(i * 2 * np.pi / num_candidates), pt_current[1] + radius * np.sin(i * 2 * np.pi / num_candidates)))
         phasor_temp = get_phasor(pt_temp)
         
         # sim_temp = cosine_similarity(phasor_normalized(phasor_measured), phasor_normalized(phasor_temp)) # direct distance/phase
@@ -200,47 +208,115 @@ def plot_hotspot_df():
 
 
 
+
+
 ###### complete simulation ######
-def func(x):
+def func1(x):
     return x**2
+
+def func2(x):
+    return np.sin(x * np.pi)
+
+def plot_antenna_position():
+    plt.scatter(ant_1[0], ant_1[1], c='green', s=10)
+    plt.scatter(ant_2[0], ant_2[1], c='green', s=10)
+    plt.scatter(ant_3[0], ant_3[1], c='green', s=10)
+    plt.scatter(ant_4[0], ant_4[1], c='green', s=10)
 
 
 def generate_trajectory():
     # generate a trajectory and sample at certain time-stamp
-    # from (0, 40) to (40, 80)
-    y = np.linspace(0, 40, 50) # len = 50, range = (0, 40)
-    x = func(y) / 40
-    return x, y + 40
+    ### type 1 ###
+    # y = np.linspace(0, 1, 50) # len = 50, range = (0, 40)
+    # x = func1(y)
+
+    ### type 2 ###
+    # x = np.linspace(0, 1, 50)
+    # y = func1(x)
+
+    ### type 3 ###
+    # x = np.linspace(0, 1, 50) # related to samples number
+    # y = func2(x)
+
+    ### type 4 ###
+    y = np.linspace(0, 1, 50) # related to samples number
+    x = func2(y)
+
+    # introducing noise
+    # for i in range(len(x)):
+    #     # x[i] = x[i] + random.uniform(-0.05, 0.05)
+    #     y[i] = y[i] + random.uniform(-0.02, 0.02)
+
+
+
+    return x - 0.3, y + 1
 
 
 def simulation_process():
     X, Y = generate_trajectory()
     plt.figure(figsize=(12,12))
-    plt.plot(X, Y)
+    plt.plot(X, Y, c='steelblue')
     plt.scatter(X, Y, c='red', s=5)
-    current_pt = np.array((0, 40))
+    # plot_antenna_position()
 
-    x_set = [X[0]]
-    y_set = [Y[0]]
+
+    current_pt = pt_1
+
+    candidate_traj_x = [X[0]]
+    candidate_traj_y = [Y[0]]
+    # print(X)
+    # print(Y)
 
     for i in range(len(X)-1):
         measure_pt = np.array((X[i+1], Y[i+1]))
-        
-        temp_pt = candidate_generator(current_pt, measure_pt, 2, 100)
+        # print(measure_pt)
+        print('-------------------')
 
+        if i == 0:
+            df = np.array((0, 0))
+        else:
+            df = np.array((X[i]-X[i-1], Y[i]-Y[i-1]))
+        print(df)
+
+        df_size = np.linalg.norm(df)
+        print(df_size) # difference in distance, actually unknown, only phase can be obtained
+
+        previewed_pt = current_pt + df # 1-order preview, linear interpolation
+ 
+
+
+        # r should be heuristic, estimate velocity, 2 * df_size
+
+        temp_pt = candidate_generator(current_pt, measure_pt, 2 * df_size, 100) # 100 particles, searching area is 0.05m circle in distance
+        # temp_pt = candidate_generator(previewed_pt, measure_pt, 2 * df_size, 100) # 100 particles, searching area is 0.05m circle in distance
+
+
+
+        matching_bound_x = [temp_pt[0], measure_pt[0]]
+        matching_bound_y = [temp_pt[1], measure_pt[1]]
+        plt.plot(matching_bound_x, matching_bound_y)
+        
+
+
+        # update current position
         current_pt = temp_pt
 
-        x_set.append(temp_pt[0])
-        y_set.append(temp_pt[1])
-        # plt.scatter(temp_pt[0], temp_pt[1], c='blue', s=5)
+        # record selected candidates trajectory
+        candidate_traj_x.append(temp_pt[0])
+        candidate_traj_y.append(temp_pt[1])
 
-    plt.plot(x_set, y_set, c='blue')
+
+
+    plt.plot(candidate_traj_x, candidate_traj_y, c='blue')
     
-    plt.show()
+    # plt.show()
+    plt.savefig('sin1_no-preview.jpg')
 
 
+
+###################################
 def synthesis_scope():
-    plt.figure(figsize=(8,8))
+    plt.figure(figsize=(10,10))
     temp_pt = generate_random_pt(pt_1, 5)
     plt.scatter(temp_pt[0], temp_pt[1], c='blue', s=20, label='current ground truth')
 
@@ -256,6 +332,13 @@ if __name__ == '__main__':
     # plot_hotspot_df()
 
     simulation_process()
+
+    
+
+
+
+
+
 
 
     
@@ -287,6 +370,9 @@ The parameter 'radius' should be big enough to cover the biggest speed of possib
 Use the best 2 of them and synthesis have a quite good performance. 
 Searching space should be circle but not square.
 Todo:
-Number of candidates/particles.
-Heuristic radius.
+Number of candidates/particles. Trade-off.
+Heuristic radius. Prepared for the possible velocity variation. Too big, noise and fluctuation. Too small, unable to track.
+If not real-time, try to reduce the updating rate of rfid reader.
+
+Knowing direction, use linear-interpolated-previewed pf. Divdided into simple and complex trajectory.
 """
